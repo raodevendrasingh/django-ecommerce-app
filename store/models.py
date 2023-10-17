1ce66e180655244c34ace5c0abf0f1774762309f
Devendra Singh Rao <dsrao1602@gmail.com>
2023-10-17 19:25:39 +0530

    Added cart and checkout functionality

--- store/models.py
+++ store/models.py
@@ -40,6 +40,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(
@@ -48,6 +60,11 @@ class OrderItem(models.Model):
         Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingDetails(models.Model):
