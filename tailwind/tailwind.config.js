a76b0190dc30fc259fe0dab9cd70af064090a861
Devendra Singh Rao <dsrao1602@gmail.com>
2023-11-22 02:49:51 +0530

    improved homepage ui

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: [ '../templates/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }                    
+    },
   },
   plugins: [],
 }
